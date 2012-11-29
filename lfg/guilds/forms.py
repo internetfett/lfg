8c09bf0b00e40a45f9e666cd3496f538999fb6af
InternetFett <internetfett@gmail.com>
2012-11-28 21:11:17 -0600

    add guildplaytime inline formset

--- lfg/guilds/forms.py
+++ lfg/guilds/forms.py
@@ -1,10 +1,32 @@
 from django import forms
+from django.forms.models import inlineformset_factory
 from django.utils.translation import ugettext_lazy as _
 from lfg.games.models import Faction, CharacterClass, CharacterSubclass, CharacterRole
-from lfg.guilds.models import Guild, GuildType
+from lfg.guilds.models import Guild, GuildType, GuildPlaytime
 from lfg.servers.models import Server
 
 class CreateGuildForm(forms.ModelForm):
+    name = forms.CharField(_('name'), required=True)
+    server = forms.ModelChoiceField(label=_('Server'), queryset=Server.objects.all(), required=True)
+    faction = forms.ModelChoiceField(label=_('Faction'), queryset=Faction.objects.all(), required=True)
+    guildtype = forms.ModelChoiceField(label=_('Guild Focus'), queryset=GuildType.objects.all(), required=True)
+    tagline = forms.CharField(label=_('Tagline'), required=True)
+    website = forms.CharField(label=_('Website'), required=False)
+    blurb = forms.CharField(label=_('Description'), required=False, widget=forms.Textarea)
+    classes = forms.ModelMultipleChoiceField(label=_('Class'), queryset=CharacterClass.objects.all(), widget=forms.CheckboxSelectMultiple)
+    subclasses = forms.ModelMultipleChoiceField(label=_('Subclass'), queryset=CharacterSubclass.objects.all(), widget=forms.CheckboxSelectMultiple)
+    roles = forms.ModelMultipleChoiceField(label=_('Role'), queryset=CharacterRole.objects.all(), widget=forms.CheckboxSelectMultiple)
+    
+    class Meta:
+       model = Guild
+       fields = ('name', 'server', 'faction', 'guildtype', 'tagline', 'website', 'blurb', 'classes', 'subclasses', 'roles')
+       exclude = ('owner', 'game')
+    
+    def clean(self):
+        cleaned_data = super(CreateGuildForm, self).clean()
+        return cleaned_data
+
+class GuildPlaytimeForm(forms.ModelForm):
     DAY_CHOICES = (
         ('1', 'Sunday'),
         ('2', 'Monday'),
@@ -14,21 +36,11 @@ class CreateGuildForm(forms.ModelForm):
         ('6', 'Friday'),
         ('7', 'Saturday'),
     )
-    name = forms.CharField(_('name'), required=True)
-    server = forms.ModelChoiceField(label=_('Server'), queryset=Server.objects.all(), required=True)
-    faction = forms.ModelChoiceField(label=_('Faction'), queryset=Faction.objects.all(), required=True)
-    guildtype = forms.ModelChoiceField(label=_('Guild Focus'), queryset=GuildType.objects.all(), required=True)
-    tagline = forms.CharField(label=_('Tagline'), required=True)
-    website = forms.CharField(label=_('Website'), required=False)
-    blurb = forms.CharField(label=_('Description'), required=False, widget=forms.Textarea)
-    classes = forms.ModelChoiceField(label=_('Class'), queryset=CharacterClass.objects.all())
-    subclasses = forms.ModelChoiceField(label=_('Subclass'), queryset=CharacterSubclass.objects.all())
-    roles = forms.ModelChoiceField(label=_('Role'), queryset=CharacterRole.objects.all())
-        
-    class Meta:
-       model = Guild
-       fields = ('name', 'server', 'faction', 'guildtype', 'tagline', 'website', 'blurb', 'classes', 'subclasses', 'roles')
+    day = forms.ChoiceField(label=_('Day'), choices=DAY_CHOICES, required=True)
+    start_time = forms.TimeField(label=_('Start Time'), required=True)
+    end_time = forms.TimeField(label=_('End Time'), required=True)
     
-    def clean(self):
-        cleaned_data = super(CreateGuildForm, self).clean()
-        return cleaned_data
+    class Meta:
+        model = GuildPlaytime
+
+GuildPlaytimeFormSet = inlineformset_factory(Guild, GuildPlaytime, extra=7, max_num=7, form=GuildPlaytimeForm)
--- lfg/guilds/views.py
+++ lfg/guilds/views.py
@@ -1,20 +1,46 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 from django.utils.decorators import method_decorator
-from django.views.generic.edit import FormView
+from django.views.generic.edit import CreateView
+from django.views.generic.simple import redirect_to
+from lfg.games.models import Game
 from lfg.guilds.models import Guild
-from lfg.guilds.forms import CreateGuildForm
+from lfg.guilds.forms import CreateGuildForm, GuildPlaytimeFormSet
 
-class CreateGuildView(FormView):
+class CreateGuildView(CreateView):
     template_name = 'guilds/create.html'
     form_class = CreateGuildForm
     success_url = '/thanks/'
     
+    def get_context_data(self, **kwargs):
+        context = super(CreateGuildView, self).get_context_data(**kwargs)
+        if self.request.POST:
+            context['formset'] = GuildPlaytimeFormSet(self.request.POST)
+        else:
+            context['formset'] = GuildPlaytimeFormSet()
+        return context
+    
     def form_valid(self, form):
-        # This method is called when valid form data has been POSTed.
-        # It should return an HttpResponse.
-        form.send_email()
-        return super(ContactView, self).form_valid(form)
-        
+        instance = form.save(commit=False)
+        instance.owner = User.objects.get(id=self.request.user.id)
+        try:
+            game = Game.objects.get(abbr=self.request.subdomain)
+            game_id = game.id
+        except:
+            game_id = 1
+        instance.game = Game.objects.get(id=game_id)
+        instance.save()
+        context = self.get_context_data()
+        formset = context['formset']
+        if formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return HttpResponseRedirect(self.success_url)
+        else:
+            return self.render_to_response(self.get_context_data(form=form))
+    
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super(CreateGuildView, self).dispatch(*args, **kwargs)
