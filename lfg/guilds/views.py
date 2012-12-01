0dbc49777cf3f82b9adbf7f36e7c5f3cbcc120f9
InternetFett <internetfett@gmail.com>
2012-11-30 23:04:31 -0600

    add view/delete and authorization to guilds

--- lfg/guilds/views.py
+++ lfg/guilds/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.http import Http404
 from django.http import HttpResponseRedirect
 from django.utils.decorators import method_decorator
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic import DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.simple import redirect_to
 
 from lfg.games.models import Game
@@ -53,13 +55,34 @@ class CreateGuildView(GuildMixin, CreateView):
     pass
 
 
+class DeleteGuildView(GuildMixin, DeleteView):
+    template_name = 'guilds/delete.html'
+
+    def get_object(self, queryset=None):
+        obj = super(DeleteGuildView, self).get_object()
+        if not obj.owner == self.request.user:
+            raise Http404
+        return obj
+
+
 class UpdateGuildView(GuildMixin, UpdateView):
     template_name = 'guilds/update.html'
 
     def get_context_data(self, **kwargs):
-        context = super(GuildMixin, self).get_context_data(**kwargs)
+        context = super(UpdateGuildView, self).get_context_data(**kwargs)
         if self.request.POST:
             context['formset'] = GuildPlaytimeFormSet(self.request.POST, instance=self.get_object())
         else:
             context['formset'] = GuildPlaytimeFormSet(instance=self.get_object())
         return context
+
+    def get_object(self, queryset=None):
+        obj = super(UpdateGuildView, self).get_object()
+        if not obj.owner == self.request.user:
+            raise Http404
+        return obj
+
+
+class GuildDetailView(DetailView):
+    template_name = 'guilds/detail.html'
+    model = Guild
--- lfg/urls.py
+++ lfg/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from lfg.guilds.views import CreateGuildView, UpdateGuildView
+from lfg.guilds.views import CreateGuildView, DeleteGuildView, UpdateGuildView, GuildDetailView
 from lfg.views import HomepageView
 
 admin.autodiscover()
@@ -10,6 +10,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', HomepageView.as_view(), name='homepage'),
     url(r'^accounts/', include('registration.backends.default.urls')),
+    url(r'^guilds/(?P<pk>\d+)/$', GuildDetailView.as_view(), name='guild_detail'),
     url(r'^guilds/create/$', CreateGuildView.as_view(), name='create_guild'),
+    url(r'^guilds/delete/(?P<pk>\d+)/$', DeleteGuildView.as_view(), name='delete_guild'),
     url(r'^guilds/update/(?P<pk>\d+)/$', UpdateGuildView.as_view(), name='update_guild'),
 )
